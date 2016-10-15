ef47bef5b2ec247a7ca4c2aa62858d9c73277d03
Ronak Bakshi <s524975@mail.nwmissouri.edu>
2016-10-14 20:27:35 -0500

    Rushes

---
+++ Bearcats_Football/Bearcats_Football/Models/Rushes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bearcats_Football.Models
+{
+    public class Rushes
+    {
+        public int count { get; set; }
+        public DateTime Date { get; set; }
+        public String opponent_team { get; set; }
+        public String player_name { get; set; }
+    }
+} 
