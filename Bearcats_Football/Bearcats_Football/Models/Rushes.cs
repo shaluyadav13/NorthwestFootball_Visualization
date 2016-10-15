dbbfa8ea36b51dd928f908d5a5e0a0cf0c39b1a3
s525078 <s525078@mail.nwmissouri.edu>
2016-10-14 20:18:06 -0500

    Rushes class pushed

---
+++ Bearcats_Football/Bearcats_Football/Models/Rushes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bearcats_Football.Models
+{
+    public class Rushes
+    {
+        public int count { get; set; }
+        public DateTime date { get; set; }
+        public string opponent_team { get; set; }
+        public string player_name { get; set; }
+    }
+}
